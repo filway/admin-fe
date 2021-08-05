36219fcb276a06d7bd22563f52c19807ebaf27b8
Zx 小小 <zxxiaoxiao@ZxdeMac-mini.local>
2021-08-05 21:56:04 +0800

    feat: pages

---
+++ mock/admin.ts
@@ -0,0 +1,23 @@
+/**
+ * @description mock admin API
+ */
+
+ export default {
+  'GET /api/admin/getUserInfo': {
+      // errno: 0,
+      // data: {
+      //     username: 'filway',
+      // },
+
+      // 模拟当前未登录
+      errno: 1,
+      message: '未登录',
+  },
+
+  'POST /api/admin/login': {
+      errno: 0,
+      data: {
+          token: 'xxxxxx',
+      },
+  },
+}
