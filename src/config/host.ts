ee73c40ac3cef5125609f8cb4c6e278519aac679
Zx 小小 <zxxiaoxiao@ZxdeMac-mini.local>
2021-08-12 01:12:55 +0800

    fix: 修改host配置

--- src/config/host.ts
+++ src/config/host.ts
@@ -7,15 +7,15 @@
  // API host
  let APIHost = '' // 默认为本地运行 mock
  if (isDevNoMock) APIHost = 'http://localhost:3003' // 本地，不用 mock
- if (isPrdDev) APIHost = 'http://182.92.168.192:8084' // 测试机
+ if (isPrdDev) APIHost = 'http://139.155.58.207:8088' // 测试机
  if (isPrd) APIHost = '' // 线上环境，用当前域名即可
 
  // h5 host
- let H5Host = 'http://182.92.168.192:8082'
- if (isPrd) H5Host = 'https://h5.imooc-lego.com'
+ let H5Host = 'http://139.155.58.207:8083'
+ if (isPrd) H5Host = 'https://h5.filway.cn'
 
  // 统计接口的 host
- let StatHost = 'http://182.92.168.192:8080'
- if (isPrd) StatHost = 'https://statistic-res.imooc-lego.com' // 线上环境
+ let StatHost = 'http://139.155.58.207:8084'
+ if (isPrd) StatHost = 'https://statistic-res.filway.cn' // 线上环境
 
  export { APIHost, H5Host, StatHost }
