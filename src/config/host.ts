b8cb851e6769b4dfcfeba17236013b50224c9d6d
Zx 小小 <zxxiaoxiao@ZxdeMac-mini.local>
2021-08-20 01:07:53 +0800

    fix: 准备部署上线

--- src/config/host.ts
+++ src/config/host.ts
@@ -8,14 +8,14 @@
  let APIHost = '' // 默认为本地运行 mock
  if (isDevNoMock) APIHost = 'http://localhost:3003' // 本地，不用 mock
  if (isPrdDev) APIHost = 'http://139.155.58.207:8088' // 测试机
- if (isPrd) APIHost = '' // 线上环境，用当前域名即可
+ if (isPrd) APIHost = 'h5admin.filway.cn' // 线上环境，用当前域名即可
 
  // h5 host
  let H5Host = 'http://139.155.58.207:8083'
- if (isPrd) H5Host = 'https://h5.filway.cn'
+ if (isPrd) H5Host = 'http://h5.filway.cn'
 
  // 统计接口的 host
  let StatHost = 'http://139.155.58.207:8084'
- if (isPrd) StatHost = 'https://statistic-res.filway.cn' // 线上环境
+ if (isPrd) StatHost = 'http://h5statistic-res.filway.cn' // 线上环境
 
  export { APIHost, H5Host, StatHost }
